9591316a0ab31d1cb5d523ba6c82d7c99ebff011
Victor dos Santos borges <victorsantos869@hotmail.com>
2018-11-19 14:42:05 -0200

    Criado a classe Posicao e testado na funcao principal (Program)

--- Projeto xadrez/Program.cs
+++ Projeto xadrez/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace Projeto_xadrez
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
+
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("A Posição é: " + P);
+
+            Console.ReadLine();
+
         }
     }
 }
