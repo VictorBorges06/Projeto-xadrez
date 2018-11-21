6a28d3a22d26da1caa5cc4b8621f3544f37c3e85
Victor Borges <victorsantos869@hotmail.com>
2018-11-21 12:55:48 -0200

    Criando a classe Posicaoxadrez

---
+++ Projeto xadrez/Xadrez (Jogo)/Posicaoxadrez.cs
@@ -0,0 +1,27 @@
+﻿using tabuleiro;
+
+namespace Xadrez__Jogo_
+{
+    class Posicaoxadrez{
+
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public Posicaoxadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao() //Método criado para converter a posição na matriz
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+
+    }
+}
