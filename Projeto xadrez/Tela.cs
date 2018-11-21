5719570d879a2d6947678837f3f800398ae815e5
Victor Borges <victorsantos869@hotmail.com>
2018-11-21 13:18:07 -0200

    Melhorando a implementacao do metodo imprimir tabuleiro

--- Projeto xadrez/Tela.cs
+++ Projeto xadrez/Tela.cs
@@ -9,17 +9,34 @@ namespace Projeto_xadrez
         {
             for (int i= 0; i<tab.linhas; i++) //Os dois 'for' foram criados para percorrer as linhas e colunas do tabuleiro
             {
+                Console.Write(8 - i + " "); //Essa linha é para melhorar a impressão do tabuleiro.
                 for (int j=0; j<tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null) //Teste feito para imprimir um traço ' - ' caso o campo (posição) n recebe peça
                     {
                         Console.Write("- ");
                     }
-                    else { 
-                    Console.Write(tab.peca(i,j)  + " "); //Para imprimir o que tem na linha ou coluna!
+                    else {
+                        imprimirPeca(tab.peca(i, j)); //Para imprimir o que tem na linha ou coluna!
+                        Console.Write(" "); 
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h"); //Essa linha é para aparecer as listas igual no tabuleiro de xadrez
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
