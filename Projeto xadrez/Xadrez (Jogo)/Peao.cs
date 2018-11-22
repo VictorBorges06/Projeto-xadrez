f3bb23e6bd4e5a600e6dac82530b395641812afe
Victor Borges <victorsantos869@hotmail.com>
2018-11-22 14:48:08 -0200

    Jogada especial en passant

--- Projeto xadrez/Xadrez (Jogo)/Peao.cs
+++ Projeto xadrez/Xadrez (Jogo)/Peao.cs
@@ -4,9 +4,12 @@ namespace Xadrez__Jogo_
 {
     class Peao : Peca{
 
-        public Peao (Tabuleiro tab, Cor cor) : base(tab, cor)
-        {
+        private PartidaDeXadrez partida;
 
+
+        public Peao (Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
+        {
+            this.partida = partida;
         }
 
         public override string ToString()
@@ -26,32 +29,46 @@ namespace Xadrez__Jogo_
            
         }
 
-        public override bool [,] movimentosPossiveis()
+        public override bool[,] movimentosPossiveis()
         {
             bool[,] mat = new bool[tab.linhas, tab.colunas];
 
             Posicao pos = new Posicao(0, 0);
 
 
-            if (cor == Cor.Branca){
+            if (cor == Cor.Branca) {
                 pos.definirValores(posicao.linha - 1, posicao.coluna);
-                if (tab.posicaoValida(pos) && livre(pos)){
+                if (tab.posicaoValida(pos) && livre(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha - 2, posicao.coluna);
-                if(tab.posicaoValida(pos) && livre(pos) && qntMovimentos == 0){
+                if (tab.posicaoValida(pos) && livre(pos) && qntMovimentos == 0) {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
-                if (tab.posicaoValida(pos) && existeInimigo(pos)){
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
-                if (tab.posicaoValida(pos) && existeInimigo(pos)){
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
+
+                //Jogada Especial En Passant peça branca
+                if (posicao.linha == 3)
+                {
+                    Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
+                    if (tab.posicaoValida(esquerda) && existeInimigo(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant) {
+                        mat[esquerda.linha - 1, esquerda.coluna] = true;
+                    }
+                
+                Posicao direita = new Posicao(posicao.linha, posicao.coluna + 1);
+                if (tab.posicaoValida(direita) && existeInimigo(direita) && tab.peca(direita) == partida.vulneravelEnPassant) {
+                    mat[direita.linha - 1, direita.coluna] = true;
+                }
             }
-            else
+         }
+            else //else das peças pretas
             {
                 pos.definirValores(posicao.linha + 1, posicao.coluna);
                 if (tab.posicaoValida(pos) && livre(pos))
@@ -72,7 +89,23 @@ namespace Xadrez__Jogo_
                 if (tab.posicaoValida(pos) && existeInimigo(pos)){
                     mat[pos.linha, pos.coluna] = true;
                 }
+                //Jogada Especial En Passant peça preta
+                if (posicao.linha == 4)
+                {
+                    Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
+                    if (tab.posicaoValida(esquerda) && existeInimigo(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant)
+                    {
+                        mat[esquerda.linha + 1, esquerda.coluna] = true;
+                    }
+
+                    Posicao direita = new Posicao(posicao.linha, posicao.coluna + 1);
+                    if (tab.posicaoValida(direita) && existeInimigo(direita) && tab.peca(direita) == partida.vulneravelEnPassant)
+                    {
+                        mat[direita.linha + 1, direita.coluna] = true;
+                    }
+                }
             }
+        
             return mat;
         }
     }
