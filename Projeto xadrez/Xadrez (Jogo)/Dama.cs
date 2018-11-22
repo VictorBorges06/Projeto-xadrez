6cc027e1315fdce49532da164709a29b4ce565e7
Victor Borges <victorsantos869@hotmail.com>
2018-11-22 12:30:47 -0200

    Inserido as outras pecas cavalo, dama, bispo e etc

---
+++ Projeto xadrez/Xadrez (Jogo)/Dama.cs
@@ -0,0 +1,128 @@
+﻿using tabuleiro;
+
+namespace Xadrez__Jogo_
+{
+    class Dama : Peca
+    { 
+
+        public Dama(Tabuleiro tab, Cor cor) : base(tab, cor)
+        { 
+        }
+        public override string ToString()
+        {
+            return "D";
+        }
+
+        private bool podeMover(Posicao pos) { 
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            
+            pos.definirValores(posicao.linha, posicao.coluna - 1); //Dama para a esquerda
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha, pos.linha - 1);
+            }
+
+
+            pos.definirValores(posicao.linha, posicao.coluna + 1); //Dama para a direita
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha, pos.linha + 1);
+            }
+
+
+
+            pos.definirValores(posicao.linha -1, posicao.coluna); //Dama para a cima
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha - 1, pos.linha);
+            }
+
+
+            pos.definirValores(posicao.linha + 1, posicao.coluna); //Dama para a baixo
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha + 1, pos.linha);
+            }
+
+            // Está lógica é para a Dama mover para o Noroeste!
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha - 1, pos.coluna - 1);
+            }
+
+            // Está lógica é para a Dama mover para o Nordeste!
+            pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha - 1, pos.coluna + 1);
+            }
+
+
+            // Está lógica é para a Dama mover para o Sudeste!
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha + 1, pos.coluna + 1);
+            }
+
+            // Está lógica é para a Dama mover para o Sudoeste!
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha + 1, pos.coluna - 1);
+            }
+
+            return mat;
+        }
+    }
+}
